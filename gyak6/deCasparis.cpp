db01dee9dc2256e31993e2ed43361d16c7b33c78
zsoltpete <pete.zsolt95@gmail.com>
2017-10-17 21:25:56 +0200

    :sparkles: De Casperis is perfect

--- gyak6/deCasparis.cpp
+++ gyak6/deCasparis.cpp
@@ -2,7 +2,8 @@
 
 //Globális elemek
 Matrix points;
-vec2 t = vec2(0,1);
+vec2 t = vec2 ( 0,1 );
+double T = 0.5;
 
 double winWidth = 600, winHeight = 600;
 
@@ -12,7 +13,7 @@ Matrix initPoints() {
     Matrix points;
     points.push_back ( vec2 ( 100,100 ) );
     points.push_back ( vec2 ( 250,500 ) );
-    points.push_back ( vec2 ( 400,500 ) );
+    points.push_back ( vec2 ( 400,575 ) );
     points.push_back ( vec2 ( 450,500 ) );
     points.push_back ( vec2 ( 500,100 ) );
     return points;
@@ -26,68 +27,115 @@ void init() {
     glEnable ( GL_POINT_SMOOTH );
     glPointSize ( 10.0 );
     glLineWidth ( 2.0 );
-    
+
     points = initPoints();
 }
 
-vec2 deCasparis(Matrix P, double t){
-  Matrix Q;
-  int n = P.size();
-  for(int i = 0; i< n;i++){
-    Q.push_back(P[i]);
-  }
-  for(int r = 1; r < n; r++){
-    for (int i = 0; i < n - r;i++){
-      vec2 point;
-      point.x = (1 - t) * ((vec2)Q[i]).x + t * ((vec2)Q[i + 1]).x;
-      point.y = (1 - t) * ((vec2)Q[i]).y + t * ((vec2)Q[i + 1]).y;
-      Q[i] = point;
-      
+vec2 deCasparis ( Matrix P, double t ) {
+    Matrix Q;
+    int n = P.size();
+    for ( int i = 0; i< n; i++ ) {
+        Q.push_back ( P[i] );
     }
-  }
-  return Q[0];
-  
+    for ( int r = 1; r < n; r++ ) {
+        for ( int i = 0; i < n - r; i++ ) {
+            vec2 point;
+            point.x = ( 1 - t ) * ( ( vec2 ) Q[i] ).x + t * ( ( vec2 ) Q[i + 1] ).x;
+            point.y = ( 1 - t ) * ( ( vec2 ) Q[i] ).y + t * ( ( vec2 ) Q[i + 1] ).y;
+            Q[i] = point;
+        }
+    }
+    return Q[0];
+
 }
 
-void printTPoints ( Matrix points ) {
+void linkDeCasparisPoints ( Matrix points, double T ) {
+    int n = points.size();
+    if ( n == 1 ) {
+        for ( int i = 0; i < n; i++ ) {
+            vec2 point1 = points[i];
+            drawPoint ( point1, green );
+        }
+    } else {
+        Matrix newPoints;
+        for ( int i = 0; i < n - 1; i++ ) {
+            vec2 point1 = points[i];
+            vec2 point2 = points[i + 1];
+            vec2 vector = point2 - point1;
+            vector *= T;
+            vector += point1;
+            drawLine ( point1, point2, blue );
+            newPoints.push_back ( vector );
+            drawPoint ( point1, green );
+        }
+        linkDeCasparisPoints ( newPoints, T );
+    }
+}
+
+void printPoints ( Matrix points ) {
     for ( int i = 0; i< points.size(); i++ ) {
         vec2 point = points[i];
-        drawPoint ( point, blue );
+        drawPoint ( point, green );
     }
+}
+
+void drawDeCasparis() {
+    glBegin ( GL_LINE_STRIP );
+    for ( double i = t.x; i < t.y; i += 0.001 ) {
+        vec2 printPoint = deCasparis ( points, i );
+        setColor ( purple );
+        glVertex2f ( printPoint.x, printPoint.y );
+    }
+    glEnd();
 }
 
 void display() {
 
     glClear ( GL_COLOR_BUFFER_BIT );
     setColor ( red );
-     glBegin ( GL_LINE_STRIP );
-     for (double i = t.x; i < t.y; i += 0.0001){
-       vec2 printPoint = deCasparis(points, i);
-        glVertex2f ( printPoint.x, printPoint.y );
-     }
-     glEnd();
-    printTPoints ( points );
+    linkDeCasparisPoints ( points, T );
+    drawDeCasparis();
+    printPoints ( points );
     glutSwapBuffers();
 }
 
-void processMouse(GLint button, GLint action, GLint xMouse, GLint yMouse) {
-	GLint i;
-	if (button == GLUT_LEFT_BUTTON && action == GLUT_DOWN)
-		if ((i = getActivePoint(points, 4, 8, xMouse, winHeight - yMouse)) != -1)
-			dragged = i;
-	if (button == GLUT_LEFT_BUTTON && action == GLUT_UP)
-		dragged = -1;
+void processMouse ( GLint button, GLint action, GLint xMouse, GLint yMouse ) {
+    GLint i;
+    if ( button == GLUT_LEFT_BUTTON && action == GLUT_DOWN )
+        if ( ( i = getActivePoint ( points, points.size(), 8, xMouse, winHeight - yMouse ) ) != -1 )
+            dragged = i;
+    if ( button == GLUT_LEFT_BUTTON && action == GLUT_UP )
+        dragged = -1;
 }
 
-void processMouseActiveMotion(GLint xMouse, GLint yMouse) {
-	GLint i;
-	if (dragged >= 0) {
-		points[dragged].x = xMouse;
-		points[dragged].y = winHeight - yMouse;
-		glutPostRedisplay();
-	}
-	std::cout << winHeight;
-	std::cout << winHeight - yMouse;
+void processMouseActiveMotion ( GLint xMouse, GLint yMouse ) {
+    GLint i;
+    if ( dragged >= 0 ) {
+        points[dragged].x = xMouse;
+        points[dragged].y = winHeight - yMouse;
+        glutPostRedisplay();
+    }
+}
+
+void keyboard ( unsigned char key, int x, int y ) {
+    switch ( key ) {
+    case 27:
+        exit ( 0 );
+        break;
+    case 'w':
+        T += 0.01;
+        if ( T >= 1.0 ) {
+            T = 1.0;
+        }
+        break;
+    case 's':
+        T -= 0.01;
+        if ( T <= 0.0 ) {
+            T = 0.0;
+        }
+        break;
+    }
+    glutPostRedisplay();
 }
 
 int main ( int argc, char** argv ) {
@@ -98,6 +146,7 @@ int main ( int argc, char** argv ) {
     glutCreateWindow ( "de casperis" );
     init();
     glutDisplayFunc ( display );
+    glutKeyboardFunc ( keyboard );
     glutMouseFunc ( processMouse );
     glutMotionFunc ( processMouseActiveMotion );
     glutMainLoop();
